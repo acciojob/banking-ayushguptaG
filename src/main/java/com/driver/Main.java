c9a79722673c73908eaa88b4c2df7857b1af30b7
Ayush Gupta <ayushgupta068@gmail.com>
2022-12-31 14:58:26 +0530

    test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-        SavingsAccount acc= new SavingsAccount("ayush", 10000, 50000,5);
-
-        acc.deposit(1000);
-        System.out.println(acc.getBalance());
+//        SavingsAccount acc= new SavingsAccount("ayush", 10000, 50000,5);
+//
+//        acc.deposit(1000);
+//        System.out.println(acc.getBalance());
 
     }
 }
